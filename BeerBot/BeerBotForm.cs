f584f8a71c7003697719dac5168622536793f64c
deepfat <deepfat@ANCILLARY>
2016-12-01 10:10:54 +0000

    top level question added

--- BeerBot/BeerBotForm.cs
+++ BeerBot/BeerBotForm.cs
@@ -8,7 +8,10 @@ using Microsoft.Bot.Builder.FormFlow;
 
 namespace BeerBot
 {
-    public enum BeerStyleOptions { Lager, IPA, Wheat };
+    public enum InitialOptions {Drink,Pub,Surprise}
+
+    public enum LocationCheck {drinkHere,goForAWalk}
+    public enum BeerStyleOptions { theUsual, Lager, IPA, Wheat, Porter, SurpriseMe };
 
     public enum BeerOptions { BlackSheep, Theakstons, Wheat};
 
@@ -21,8 +24,17 @@ namespace BeerBot
         public static IForm<BeerBotForm> BuildForm()
         {
             return new FormBuilder<BeerBotForm>()
-                    .Message("What do you fancy?!")
+                    .Message("It looks like you are in <Location>, would you like a drink here or do you fancy a walk?")
+                    .Message("What style of Beer do you fancy?")
+                    .Message()
                     .Build();
+
+/* Hello User how can I help
+ * 1. Recommend a great drink
+ * 2. Recommend a good pub
+ * 3. Surprise me!
+ * 
+ */ 
         }
     }
 }
